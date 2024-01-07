521549393874be070ca1d1f247a2df292ad3e9a9
FaridaYasserYoussef <farida.yasser510@gmail.com>
2024-01-07 19:39:20 +0200

    from 1 to 9

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import RoomView
 from .views import CreateRoomView
-from .views import GetRoom
+from .views import GetRoom, JoinRoom, UserInRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view())
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view())
+
+
 
 ]
 
